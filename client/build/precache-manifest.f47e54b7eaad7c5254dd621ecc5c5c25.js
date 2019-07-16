ec1f14d5b3a8c3ab3fbc42700d1006cf13005c12
sinedd777 <gaurang.singh@oracle.com>
2019-07-16 14:55:24 +0530

    PreFinal build#4

---
+++ client/build/precache-manifest.f47e54b7eaad7c5254dd621ecc5c5c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66882f86f033e336918ff08daed7e1fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b681eae6c09855e07af",
+    "url": "/static/css/main.7c09ee53.chunk.css"
+  },
+  {
+    "revision": "2469d4dcb47df1ef0e45",
+    "url": "/static/js/2.da6df4d3.chunk.js"
+  },
+  {
+    "revision": "7b681eae6c09855e07af",
+    "url": "/static/js/main.0a146031.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
